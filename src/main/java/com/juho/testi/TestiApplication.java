c6f151a892fdbb7846669b80234d0564809d625a
Juho Toivonen <juho.toivonen@myy.haaga-helia.fi>
2020-04-16 14:14:14 +0300

    eka testi

---
+++ src/main/java/com/juho/testi/TestiApplication.java
@@ -0,0 +1,13 @@
+package com.juho.testi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/juho/testi/TestiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juho.testi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
