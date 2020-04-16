c15c005cfd761e783368c6d99634c84f7f8be9cb
Juho Toivonen <juho.toivonen@myy.haaga-helia.fi>
2020-04-16 14:57:27 +0300

    testi6

--- src/main/java/com/juho/testi/TestiApplication.java
+++ src/main/java/com/juho/testi/TestiApplication.java
@@ -2,6 +2,12 @@ package com.juho.testi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+
+import com.juho.testi.domain.*;
 
 @SpringBootApplication
 public class TestiApplication {
@@ -9,5 +15,22 @@ public class TestiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TestiApplication.class, args);
 	}
+	
+	// Loggeri
+	private static final Logger log = LoggerFactory.getLogger(TestiApplication.class);
+	
+	// Esimerkkikysymysten lisääminen tietokantaan
+	@Bean
+	public CommandLineRunner questionDemo(RadioRepository rqRepository) {
+		return (args) -> {
+			
+			log.info("save demo radio questions to db");
+			rqRepository.save(new Radio("Mitä kuuluu?"));
+			rqRepository.save(new Radio("Miten menee?"));
+			rqRepository.save(new Radio("Moi?"));
+		};
+	}
+	
+	
 
 }
